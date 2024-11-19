5a6cde86fbe9e44e90b18a8f6080317bb39ba730
thibaulttostain <thibault.tostain.01@gmail.com>
2024-11-19 12:30:06 +0100

    nv commit

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,34 +1,41 @@
-# conf.py
+# Configuration file for the Sphinx documentation builder.
 
-# -- Project information -----------------------------------------------------
+# -- Project information
 
-project = 'Nom de votre projet'
-author = 'Votre nom ou organisation'
-release = '1.0.0'
+import sphinx
+import sphinx_rtd_theme
+import sys
+import os
 
-# -- General configuration ---------------------------------------------------
+project = 'Lumache'
+copyright = '2021, Graziella'
+author = 'Graziella'
+
+release = '0.1'
+version = '0.1.0'
+
+# -- General configuration
 
 extensions = [
-    'sphinx.ext.autodoc',  # Extension pour générer la documentation à partir des docstrings
-    'sphinx.ext.viewcode', # Ajoute des liens vers le code source
-    'sphinx_copybutton',   # Ajoute les boutons "Copier" dans les blocs de code
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+    'sphinx_copybutton',
 ]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-# -- Options for HTML output -------------------------------------------------
+# -- Options for HTML output
 
-html_theme = 'alabaster'  # Vous pouvez remplacer par un autre thème comme "sphinx_rtd_theme"
-html_static_path = ['_static']
+html_theme = 'sphinx_rtd_theme'
 
-# -- sphinx-copybutton configuration -----------------------------------------
-
-# Ignorer certaines lignes spécifiques dans les blocs de code
-copybutton_prompt_text = ">>> "  # Ignore les lignes commençant par ">>>"
-copybutton_only_copy_prompt_lines = False  # Copie tout, pas uniquement les lignes commençant par le prompt
-
-# Si besoin de styles CSS spécifiques pour le bouton (optionnel)
-html_css_files = [
-    'custom.css',  # Placez ce fichier dans le répertoire "_static" si nécessaire
-]
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
